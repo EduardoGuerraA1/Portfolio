d885e7811ebc04c1152e7ebd943f7275f4af936d
EduardoGuerraA1 <114944026+EduardoGuerraA1@users.noreply.github.com>
2022-11-22 17:57:26 -0600

    commit

---
+++ js/index.js
@@ -0,0 +1,40 @@
+window.sr = ScrollReveal();
+
+
+    sr.reveal('.home', {
+        duration: 500
+        
+    })
+
+    sr.reveal('.about-me', {
+        duration: 3000
+    })
+
+    sr.reveal('.skills', {
+        duration: 3000
+    })
+
+    sr.reveal('.hobbies', {
+        duration: 3000
+    })
+
+    
+    sr.reveal('.formation', {
+        duration: 3000
+        
+    })
+
+    sr.reveal('.formation-icons', {
+        duration: 3000
+        
+    })
+  
+    sr.reveal('.projects', {
+        duration: 3000
+        
+    })
+
+    sr.reveal('.project-box', {
+        duration: 3000
+        
+    })
